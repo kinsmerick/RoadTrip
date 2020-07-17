6fd4110b4e4d7f801a53f432069852657a0fc980
Autumn Raines <xomies@gmail.com>
2020-07-17 18:32:59 -0500

    2d Character Controller
    Began a scene setup and the start of a character controller and interactive items.

---
+++ Assets/Scripts/ExplorationSystem/Interactable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+  public bool canInteract = false;
+  private bool interacting = false;
+  public GameObject testInteract;
+
+  public CharacterController playerControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      playerControl = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(canInteract && !interacting && (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump")) ){
+        interacting = true;
+        playerControl.canWalk = false;
+        testInteract.SetActive(true);
+      }
+      else if(interacting && (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump")) ){
+        interacting = false;
+        playerControl.canWalk = true;
+        testInteract.SetActive(false);
+      }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+      if(other.tag == "Observer"){
+        canInteract = true;
+      }
+    }
+
+
+    void OnTriggerExit2D(Collider2D other){
+      if(other.tag == "Observer"){
+        canInteract = false;
+      }
+    }
+
+}
