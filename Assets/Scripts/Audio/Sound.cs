a9f0ccc02b06fd495a833b28f17f9e44fdcf9006
Kinsey E <kinsmerick@gmail.com>
2020-08-03 18:22:43 -0500

    Created Sound scriptable objects, AudioManager class, & test sounds
    AudioManager class, which is attached to an AudioManager gameobject, creates YarnCommands "PlaySound" and "StopSound" which allow sounds to be started and stopped from Yarn (i.e. rain sfx). It also has method playCharacterSound that speaks with the DialogueManager to find the current speaker, gets the speaker's Character Manager's active sound, and then plays it. This method is called by the Yarn Dialogue UI script's On Line Start method. Also added to the CharacterManager class an array of Sound scriptable objects. Each character will have at least one sound, but certain characters may have several (like Daniella having a normal and distressed sound). Thus the CharacterManager class also adds a new Yarn command, SetSound, which can allow the character's active sound to be changed between the Sound objects in their array via Yarn. The first Sound in every character's array is automatically set to be the active sound on Awake, so if a character only has one sound, they never need a SetSound command call.
    
    In addition to all that, there are now Sound scriptable objects. These contain an audio clip, a name, and then a couple parameters that directly map onto an Audio Source's variables (Loop, Play on Awake, Volume). This way we can easily make an object for each sound in the game. The AudioManager instantiates children gameobjects at the top of the game that each have one audio source component according to the set array of sounds you give the AudioManager in the editor. If you want a sound (including music) to be played in a scene, you have to add it to the AudioManager's array of sounds.
    
    That's about it fam. Other than that, updated the character prefab for Daniella with a test character sound and created a filler background music sound that is currently a Lizzo x Stardew Valley mashup.

---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Scriptable object that instantiates an AudioSource with the settings defined by
+//the user in the inspector. It contains methods to start and stop the AudioSource's playing.
+
+[CreateAssetMenu(fileName = "New Sound Object", menuName = "SoundObject")]
+public class Sound : ScriptableObject
+{
+    //The AudioSource for the Sound, which communicates with the scene's
+    //AudioListener.
+
+    private AudioSource _audioSource;
+
+    /*The following variables are set within the inspector and then passed into
+     audio sources that are instantiated on Start(). They reflect the attributes
+     of an AudioSource component, but not all of its variables are included here
+     for ease of use/comprehension by an individual without advanced sound knowledge.
+
+     clipName: a string that will be used by the AudioManager to play this Sound
+     clip: the Sound's audio file
+     volume: a float representing the volume the audio file will be played at
+     loop: a bool representing whether or not the Sound should play on loop
+
+     playOnStart: a bool representing whether or not the Sound should play on Awake.*/
+
+    public string clipName;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    public bool loop;
+    public bool playOnAwake;
+
+    //A constructor that sets a given AudioSource (instantiated in
+    //AudioManager's Start()) to the values set in the inspector.
+
+    public void setSource(AudioSource source)
+    {
+        _audioSource = source;
+        _audioSource.clip = clip;
+        _audioSource.volume = volume;
+        _audioSource.loop = loop;
+        _audioSource.playOnAwake = playOnAwake;
+
+    }//end setSource method
+
+    //calls the AudioSource's Play method if it isn't already playing
+    public void play()
+    {
+        if (!_audioSource.isPlaying)
+        {
+            _audioSource.Play();
+        }//end if
+
+    }//end play method
+
+    //calls the AudioSource's Stop method if it is playing
+    public void stop()
+    {
+        if (_audioSource.isPlaying)
+        {
+            _audioSource.Stop();
+        }//end if
+
+    }//end Stop method
+
+}//end Sound scriptable object
