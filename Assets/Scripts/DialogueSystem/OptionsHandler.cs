776155150724090c714416bb1f99f6cc6547c8ed
Kinsey E <kinsmerick@gmail.com>
2020-08-10 20:04:42 -0500

    Finished StartMenu, added audio, updated Sound.cs, updated options
    Imported Plum's audio assets from beta sprint 1. Adjusted Sound.cs to include an enum for the type of audio (music, character, sfx) so that the volume settings can adjust the sound object's volume accordingly. Added the rest of the pop-ups to the start menu and fixed button navigation for them all. Linked a (currently empty) hover sfx to play whenever a button is selected (including the options buttons in the dialogue system). Adjusted the button navigation system in OptionsHandler. Added ExitGame.cs and ToggleOptionText.cs which allow for a button to exit the game .exe and for toggle UI components' label text to change in accordance to their bool value. Created Tooltip.cs which changes the tooltip text on the StartMenu according to what button is selected. Updated Daniella's character prefabs to include her internal sound.

--- Assets/Scripts/DialogueSystem/OptionsHandler.cs
+++ Assets/Scripts/DialogueSystem/OptionsHandler.cs
@@ -5,7 +5,7 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using TMPro;
 
-/* OptionTextDisplayHandler implements three interfaces, one for pointer enter and then select and deselect.
+/* OptionsHandler implements three interfaces, one for pointer enter and then select and deselect.
  * These events set the selected object in the Event system and the Option text bubbles' text color, respectively.
  * The Options buttons' colors are handled by the Button component, but the text needs to change color to maintain
  * readability when the images change color. The color, for now, can be set in the Editor to be Daniella's character
@@ -13,20 +13,25 @@ using TMPro;
 
 public class OptionsHandler : MonoBehaviour, IPointerEnterHandler, ISelectHandler, IDeselectHandler
 {    
-    [Tooltip("Set this to Daniella's character color.")]
+    [Tooltip("Set this to the desired hover text character color.")]
     public Color hoverTextColor;
 
     private TextMeshProUGUI _tmp;
     private Color _standardTextColor;
     private EventSystem _eventSystem;
-    private Button _button;
+    private AudioManager _audManager;
 
-    // Start is called before the first frame update
-    //Gets the option's TMP component and stores the set starting text color for it.
-    void Start()
+
+    private void Awake()
     {
         _tmp = this.GetComponentInChildren<TextMeshProUGUI>();
-        _button = this.GetComponent<Button>();
+        _audManager = FindObjectOfType<AudioManager>();
+    }
+
+    // Start is called before the first frame update
+    // Stores the set starting text color for of text mesh pro component
+    void Start()
+    {
 
         if(_tmp != null)
         {
@@ -39,21 +44,23 @@ public class OptionsHandler : MonoBehaviour, IPointerEnterHandler, ISelectHandle
     }
 
     //Each time the options bubbles are enabled, it grabs the current event system. It also makes sure that
-    //the scene does not start with a button selected, so that, if players are spamming continue,
-    //they won't accidentally select a choice before they see it.
+    //if the gameobject has the name Option in it, the scene does not start with a button selected, so that,
+    //if players are spamming continue, they won't accidentally select a choice before they see it. But, if
+    //this is any other sort of button, it won't set the event to inactive such that the UI menu will still
+    //go to the set start button upon activation.
 
     private void OnEnable()
     {
         _eventSystem = EventSystem.current;
-        if (_eventSystem.currentSelectedGameObject == this.gameObject)
+        if ( _eventSystem.currentSelectedGameObject == this.gameObject && this.gameObject.name.Contains("Option"))
         {
             _eventSystem.SetSelectedGameObject(null);
         }
     }
 
     //On update, it checks to see if this object is selected. It updates the color of the text accordingly.
-    //It also checks for whether or not this is Option 1 and current event system has no object selected. If
-    //both are true, it will listen for key inputs from the player if the player wants to again select an option
+    //It also checks for whether or not this current event system has no object selected. If
+    //true, it will listen for key inputs from the player if the player wants to again select an option
     //with the keyboard. This may seem redundant, as OnSelect and OnDeselect change the color, but there are weird edge
     //cases that can happen if the player simultaneously uses the mouse and the keyboard to select options,
     //so the check in Update handles those.
@@ -68,12 +75,12 @@ public class OptionsHandler : MonoBehaviour, IPointerEnterHandler, ISelectHandle
             _tmp.color = _standardTextColor;
         }
 
-        //If the user has clicked away from the buttons, and this component is attached to the first options button,
-        //then it listens for a directional key press. If it's pressed, it selects this option bubble.
+        //If the user has clicked away from the buttons, and this component is attached to the last selected button,
+        //then it listens for a directional key press. If it's pressed, it selects an option bubble.
         //This is so that if the user clicks with the mouse but then decides they want to use the keys to navigate,
         //the event system will recognize the buttons as the current event again.
 
-        if (this.gameObject.name.Contains("1") && _eventSystem.currentSelectedGameObject == null)
+        if (_eventSystem.currentSelectedGameObject == null)
         {
             if(Input.GetButton("Horizontal") || Input.GetButton("Vertical")){
 
@@ -93,6 +100,7 @@ public class OptionsHandler : MonoBehaviour, IPointerEnterHandler, ISelectHandle
     public void OnSelect(BaseEventData data)
     {
         _tmp.color = hoverTextColor;
+        _audManager.playSound("Start_Menu_Button_Hover");
     }
 
     //when an object is deselected, change its color to normal
