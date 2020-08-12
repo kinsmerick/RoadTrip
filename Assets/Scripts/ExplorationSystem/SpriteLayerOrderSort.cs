14eaffdec2c3aa32094574a136feb1cd1ed56179
Autumn Raines <xomies@gmail.com>
2020-08-11 21:38:19 -0500

    Intermission work, sprite layer fix
    Added a script to dynamically change sprite order layers depending where the player is

---
+++ Assets/Scripts/ExplorationSystem/SpriteLayerOrderSort.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLayerOrderSort : MonoBehaviour
+{
+
+  public GameObject player;
+
+  public int aboveValue = 2;
+  public int belowValue = -2;
+
+  private SpriteRenderer theRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        theRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(player.transform.position.y > transform.position.y){
+        theRenderer.sortingOrder = aboveValue;
+      }
+      else{
+        theRenderer.sortingOrder = belowValue;
+      }
+    }
+}
