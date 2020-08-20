b3c6f6f3cdaaae7905a635113de47d9c13f4c943
Roscoe <roscoerappaport@gmail.com>
2020-08-20 14:56:42 -0700

    Oh god oh jeez oh no
    There are animations here

---
+++ Assets/Scripts/CarScenes/Despawn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+public class Despawn : MonoBehaviour
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public float TimeTillDelete = 5f;
+    private float timer;
+
+    //Makes a repeating instance of the events called l8r
+    void Start()
+
+    {
+        timer = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time - timer > TimeTillDelete)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
