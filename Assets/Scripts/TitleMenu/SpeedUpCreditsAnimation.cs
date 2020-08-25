f2f5578c0f37e2c7f5c9a5287460b308dd879a43
Autumn Raines <xomies@gmail.com>
2020-08-25 16:04:45 -0500

    Added script to optinally speed up credits
    Credits screen will scroll faster if you're holding down space or click

---
+++ Assets/Scripts/TitleMenu/SpeedUpCreditsAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUpCreditsAnimation : MonoBehaviour
+{
+
+  public float fastSpeedAmount = 4;
+  private Animator credAnim;
+  private float defaultSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        credAnim = GetComponent<Animator>();
+        defaultSpeed = credAnim.speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump"))
+          {
+              credAnim.speed = fastSpeedAmount;
+          }
+      else if(Input.GetButtonUp("Fire1") || Input.GetButtonUp("Jump"))
+          {
+              credAnim.speed = defaultSpeed;
+          }
+
+    }
+}
