3116990a742d8c94a16cede14432bfe16776fe33
Autumn Raines <xomies@gmail.com>
2020-09-30 17:36:07 -0500

    Facepunch steam integration
    Detours can connect to steam now.

---
+++ Assets/Scripts/SteamScripts/SteamIntegrationScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamIntegrationScript : MonoBehaviour
+{
+
+  public bool steamActive = false;
+
+  public bool logOutSteam = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        try
+        {
+        	Steamworks.SteamClient.Init( 1427300, true );
+          steamActive = true;
+        }
+        catch ( System.Exception e )
+        {
+        	Debug.Log("Error. No Steam?" + e);
+          steamActive = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(steamActive){
+        Steamworks.SteamClient.RunCallbacks();
+      }
+
+      if(logOutSteam){
+        Steamworks.SteamClient.Shutdown();
+        logOutSteam  = false;
+        steamActive = false;
+      }
+
+    }
+
+
+    void OnApplicationQuit(){
+      if(steamActive){
+        Steamworks.SteamClient.Shutdown();
+      }
+    }
+
+
+}
